f23539d2dc260605e175468b3dcc4c91e3201e73
Немченко Виталий 23582 RU <vitaliy.nemchenko@dpd.ru>
2023-06-20 15:24:27 +0300

    убрал создания таблиц из ран

--- run.py
+++ run.py
@@ -6,13 +6,13 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 from config import TELEGRAM_BOT_TOKEN, logger
 from chatgpt_utils import get_nutrition_info
 from sql import (get_data_from_db, add_entry,
-                 get_user, add_user, update_payment_date, create_users_table, create_nutrition_table, datetime)
+                 get_user, add_user, update_payment_date, datetime)
 
 
 def start(update: Update, context: CallbackContext) -> None:
     user_id = update.effective_user.id
     logger.info(f'function start started with {user_id=}')
-    user = get_user(create_users_table(), user_id)
+    user = get_user(user_id)
 
     if user:
         last_payment_date = user['last_payment_date']
@@ -22,7 +22,7 @@ def start(update: Update, context: CallbackContext) -> None:
             reply_markup = InlineKeyboardMarkup(keyboard)
             context.bot.send_message(chat_id=update.effective_chat.id, text='Здорово, вы пользуетесь моей помощью уже неделю! Как вам? Если вам нравится этот сервис, задонатьте пожалуйста на оплату сервера')
     else:
-        add_user(create_users_table(), user_id)
+        add_user(user_id)
         context.bot.send_message(chat_id=update.effective_chat.id, text='Привет, начнем подсчет калорий! Просто пиши то, что ты поел, чтобы не забыть, а если захочешь узнать итог, отправь дату в формате ДД.ММ.ГГ')
 
 
@@ -32,12 +32,12 @@ def process_message(update: Update, context: CallbackContext) -> None:
 
     if re.search(r'\b\d{2}\.\d{2}\.\d{2}\b', message_text):
         logger.info(f'function process_message started with date founded in  {message_text=}')
-        data = get_data_from_db(create_nutrition_table(), user_id, message_text)
+        data = get_data_from_db(user_id, message_text)
         context.bot.send_message(chat_id=update.effective_chat.id, text=data)
     else:
         json_data = get_nutrition_info(message_text)
         if json_data:
-            message = add_entry(create_nutrition_table(), user_id, json_data)
+            message = add_entry(user_id, json_data)
         else:
             message = f'Извините, не удалось подсчитать калории, попробуйте сформулировать фразу иначе'
 
@@ -53,7 +53,7 @@ def button(update: Update, context: CallbackContext) -> None:
         donate_process = True
         # after successful payment you should call update_payment_date function to update payment date for user
         if donate_process:
-            update_payment_date(create_users_table(), user_id)
+            update_payment_date(user_id)
             context.bot.send_message(chat_id=update.effective_chat.id, text="Спасибо!")
 
 
