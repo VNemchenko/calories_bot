ac78544489304ffc57025908ee629e809331cc08
Немченко Виталий 23582 RU <vitaliy.nemchenko@dpd.ru>
2023-06-28 16:18:40 +0300

    изменил деплой

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,2 @@
-#!/bin/bash
-
-CONTAINER_NAME=calories_bot
-
-# Останавливаем и удаляем контейнер, если он существует
-if [ $(docker ps -a -q -f name=$CONTAINER_NAME) ]; then
-    docker stop $CONTAINER_NAME
-    docker rm $CONTAINER_NAME
-fi
-
-docker build -t $CONTAINER_NAME .
-docker run --env-file .env -d --restart unless-stopped --name $CONTAINER_NAME -v /var/log/my_logs:/app/logs $CONTAINER_NAME
-
+docker build -t calories_bot .
+docker run --env-file .env -d calories_bot
